4dad30addf6a0404b0bf30de356489f39557df5e
solidsnake80 <98474700+solidsnake80@users.noreply.github.com>
2022-02-26 15:47:46 +0530

    Create array_sum.java

---
+++ experiment2/array_sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class array_sum {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int i,n,sum=0;
+        System.out.println("Enter the number of elements:");
+        n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the elements:");
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            sum=sum+a[i];
+        }
+        System.out.println("Sum of "+n+" elements is " + sum);
+    }
+}
